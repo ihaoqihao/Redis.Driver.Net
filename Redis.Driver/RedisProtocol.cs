80179e1ea289a0068f9f9cc725b29fa001acb151
洪家旺 <jon.hong@qq.com>
2013-03-08 22:55:10 +0800

    Redis.Driver/RedisProtocol.cs

--- Redis.Driver/RedisProtocol.cs
+++ Redis.Driver/RedisProtocol.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Sodao.FastSocket.Client.Protocol;
+using System.Text;
 
 namespace Redis.Driver
 {
@@ -51,6 +52,21 @@ namespace Redis.Driver
         /// <returns></returns>
         private StatusReply FindStatus(byte[] buffer, out int readed)
         {
+            int length = buffer.Length;
+            if (length < 4)
+            {
+                readed = 0;
+                return null;
+            }
+            for (int i = 1; i < length; i++)
+            {
+                if (buffer[i] == 13 && i + 1 < length && buffer[i + 1] == 10)
+                {
+                    readed = i + 1;
+                    return new StatusReply(Encoding.UTF8.GetString(buffer, 1, i - 1));
+                }
+            }
+
             readed = 0;
             return null;
         }
@@ -62,6 +78,21 @@ namespace Redis.Driver
         /// <returns></returns>
         private ErrorReply FindError(byte[] buffer, out int readed)
         {
+            int length = buffer.Length;
+            if (length < 4)
+            {
+                readed = 0;
+                return null;
+            }
+            for (int i = 1; i < length; i++)
+            {
+                if (buffer[i] == 13 && i + 1 < length && buffer[i + 1] == 10)
+                {
+                    readed = i + 1;
+                    return new ErrorReply(Encoding.UTF8.GetString(buffer, 1, i - 1));
+                }
+            }
+
             readed = 0;
             return null;
         }
@@ -73,6 +104,25 @@ namespace Redis.Driver
         /// <returns></returns>
         private IntegerReply FindInteger(byte[] buffer, out int readed)
         {
+            int length = buffer.Length;
+            if (length < 4)
+            {
+                readed = 0;
+                return null;
+            }
+            for (int i = 1; i < length; i++)
+            {
+                if (buffer[i] == 13 && i + 1 < length && buffer[i + 1] == 10)
+                {
+                    readed = i + 1;
+                    int value = 0;
+                    if (!int.TryParse(Encoding.UTF8.GetString(buffer, 1, i - 1), out value))
+                        throw new BadProtocolException();
+
+                    return new IntegerReply(value);
+                }
+            }
+
             readed = 0;
             return null;
         }
@@ -82,8 +132,45 @@ namespace Redis.Driver
         /// <param name="buffer"></param>
         /// <param name="readed"></param>
         /// <returns></returns>
-        private IntegerReply FindBulk(byte[] buffer, out int readed)
+        private BulkReplies FindBulk(byte[] buffer, out int readed)
         {
+            int length = buffer.Length;
+            if (length < 3)
+            {
+                readed = 0;
+                return null;
+            }
+            //nil
+            if (buffer[1] == '-' && buffer[2] == '1')
+            {
+                readed = 3;
+                return new BulkReplies(null);
+            }
+
+            if (length < 4)
+            {
+                readed = 0;
+                return null;
+            }
+            for (int i = 1; i < length; i++)
+            {
+                if (buffer[i] == 13 && i + 1 < length && buffer[i + 1] == 10)
+                {
+                    int payloadLength = 0;
+                    if (!int.TryParse(Encoding.UTF8.GetString(buffer, 1, i - 1), out payloadLength))
+                        throw new BadProtocolException();
+
+                    var leastLength = payloadLength + i + 4;
+                    if (length >= leastLength)
+                    {
+                        readed = leastLength;
+                        var payload = new byte[payloadLength];
+                        Buffer.BlockCopy(buffer, i + 2, payload, 0, payloadLength);
+                        return new BulkReplies(payload);
+                    }
+                }
+            }
+
             readed = 0;
             return null;
         }
